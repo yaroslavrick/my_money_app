cc4dde838a89437c02514afa8c5899ed53621dfb
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-01-17 18:03:35 +0200

    added validations for Category and Operation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   has_many :operations
+  validates :name, presence: true
+  # Назва категорії має бути унікальною (не може бути двох категорій з
+  #    однаковими назвами)
+  validates :name, uniqueness: true
+  # Опис категорії є обов’язковою властивістю (не nil)
+  validates :description, presence: true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,11 @@
 class Operation < ApplicationRecord
   belongs_to :category
+
+  #   Сума операції – обов’язкова властивість, а також значення – число, яке
+  #    більше за 0
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  #   Дата операції – обов’язкова властивість
+  validates :odate, presence: true
+  #   Короткий опис – обов’язкова властивість
+  validates :description, presence: true
 end
