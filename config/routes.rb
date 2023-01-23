068f3da00a4c4e0b32a7131ea4933367f7779325
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-01-23 22:45:59 +0200

    Added form bone to reports/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'reports/index'
-  get 'reports/report_by_category'
-  get 'reports/report_by_dates'
+  get 'reports/index', to: 'reports#index'
+  get 'reports/report_by_category', to: 'reports#report_by_category'
+  get 'reports/report_by_dates', to: 'reports#report_by_dates'
   # get 'main/index'
-  root "main#index"
+  root 'main#index'
   resources :operations
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
