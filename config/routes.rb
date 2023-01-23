ffbbd726b2f2c5960efd9ed049a59b7161168586
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-01-23 21:39:55 +0200

    added Main Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'main/index'
+  root "main#index"
   resources :operations
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
