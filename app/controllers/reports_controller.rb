a692fc6781ee7aa298b2fbdc99f546f43dc0e909
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-01-24 16:47:08 +0200

    Added methods logic to ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,9 +3,12 @@ class ReportsController < ApplicationController
   end
 
   def report_by_category
-    # Category.pluck(:created_at)
+    # @operations = Operation.all
+    @operations = Operation.where("category_id = ?", params["operation"]["category_id"]) unless params["operation"]["category_id"].empty?
   end
 
   def report_by_dates
+    @operations = Operation.where("odate >= :date_from AND odate <= :date_to",
+                                  { date_from: params["filter"]["date-from"].to_date, date_to: params["filter"]["date-to"].to_date })
   end
 end
