408ab5e9919c6332fdc21da1894e4e4b91d8461d
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-01-31 12:34:46 +0200

    added "search by activity" to reports_by_date

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,7 +8,7 @@ class ReportsController < ApplicationController
   end
 
   def report_by_dates
-    @operations = Operation.where("odate >= :date_from AND odate <= :date_to",
-                                  { date_from: params["filter"]["date-from"].to_date, date_to: params["filter"]["date-to"].to_date })
+    @operations = Operation.where("odate >= :date_from AND odate <= :date_to AND activity_id = :activity",
+                                  { date_from: params["filter"]["date-from"].to_date, date_to: params["filter"]["date-to"].to_date, activity: params["activity"]["activity_id"]})
   end
 end
