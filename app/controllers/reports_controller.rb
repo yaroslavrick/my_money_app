2a92a0e1b56c59a4bb640c21c453ce6dd36a7e37
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-01-31 18:39:05 +0200

    added canvas to report_by_dates.html.erb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,10 +5,20 @@ class ReportsController < ApplicationController
   def report_by_category
     # @operations = Operation.where("category_id = ?", params["operation"]["category_id"]) unless params["operation"]["category_id"].empty?
     @operations = Operation.where("category_id = :category AND activity_id = :activity", { category: params["operation"]["category_id"], activity: params["activity"]["activity_id"] })
+
+
   end
 
   def report_by_dates
     @operations = Operation.where("odate >= :date_from AND odate <= :date_to AND activity_id = :activity",
-                                  { date_from: params["filter"]["date-from"].to_date, date_to: params["filter"]["date-to"].to_date, activity: params["activity"]["activity_id"]})
+                                  { date_from: params["filter"]["date-from"].to_date,
+                                    date_to: params["filter"]["date-to"].to_date,
+                                    activity: params["activity"]["activity_id"]
+                                  }
+    )
+
+    operations_data = @operations.map { |o| [o.amount, o.odate.to_s] }
+    @dates = operations_data.map { |e| e[1]}
+    @amounts = operations_data.map { |e| e[0]}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'ffaker'
+
 # Generating activities:
 debit = Activity.create(atype: "debit")
 credit = Activity.create(atype: "credit")
@@ -15,3 +17,17 @@ categ1 = Category.create(name: "phones", description: "cell and mobile phones")
 categ2 = Category.create(name: "laptops", description: "small computers")
 
 # Generating Operations:
+100.times do
+  Operation.create(amount: rand(5.0..2000.0),
+                   odate: (Date.today - rand(90)),
+                   description: FFaker::Lorem.word,
+                   activity_id: rand(1..Activity.all.count),
+                   category_id: rand(1..Category.all.count)
+                   )
+end
+
+# t.decimal "amount"
+# t.datetime "odate"
+# t.string "description"
+# t.integer "activity_id"
+# t.integer "category_id"
