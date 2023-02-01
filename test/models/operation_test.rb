3717b6af96f6c64af54aa59d0c1f0201ba139ae8
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-01 23:28:56 +0200

    fixed test

--- test/models/operation_test.rb
+++ test/models/operation_test.rb
@@ -1,14 +1,35 @@
 require "test_helper"
 
 class OperationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @category = Category.create(
+      name: "phones",
+      description: "cell and mobile phones"
+    )
+  end
+
   test "return true if everything is okay" do
     new_operation = Operation.new(amount: 0.1685620424667173e4,
                                   odate: Time.now.to_s,
                                   description: "Test description",
-                                  category_id: 2)
+                                  category_id: Category.last.id)
+    assert(new_operation.valid?)
+  end
+
+  test "return true if everything is okay with 'setup' method" do
+    new_operation = Operation.new(amount: 0.1685620424667173e4,
+                                  odate: Time.now.to_s,
+                                  description: "Test description",
+                                  category_id: @category.id)
+    assert(new_operation.valid?)
+  end
+
+  test "return true if everything is okay with 'create!' method" do
+    new_operation = Operation.create!(amount: 0.1685620424667173e4,
+                                  odate: Time.now.to_s,
+                                  description: "Test description",
+                                  category_id: @category.id)
     assert(new_operation.valid?)
   end
 
