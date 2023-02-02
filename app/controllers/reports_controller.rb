582dd04b8b541d1c395b4d03793a4b671d0d1695
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-02 13:55:04 +0200

    added group operations method

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,9 +10,11 @@ class ReportsController < ApplicationController
 
   def report_by_category
     @operations = Operation.where("category_id = ?", params["operation"]["category_id"]) unless params["operation"]["category_id"].empty?
+    # or:
     # @operations = Operation.where("category_id = :category", { category: params["operation"]["category_id"]})
 
     @category = Category.find(params["operation"]["category_id"]).name
+    group_operations
     create_report
   end
 
@@ -21,17 +23,19 @@ class ReportsController < ApplicationController
                                   { date_from: params["filter"]["date-from"].to_date,
                                     date_to: params["filter"]["date-to"].to_date }
     )
-    @operations_grouped = @operations.group("strftime('%Y-%m-%d', odate)").sum("amount")
-     # .group("strftime('%Y-%m-%d 00:00:00 UTC', created_at, '-6 days', 'weekday 0')")
+    group_operations
     create_report
   end
 
   private
+  def group_operations
+    @operations_grouped = @operations.group("strftime('%Y-%m-%d', odate)").sum("amount")
+  end
   def create_report
     # operations_data = @operations.map { |o| [o.amount, o.odate] }
     # @amounts = operations_data.map { |e| e[0]}
     # @dates = operations_data.map { |e| e[1]}
-    @amounts = @operations_grouped.map { |e| e[1]}
     @dates = @operations_grouped.map { |e| e[0]}
+    @amounts = @operations_grouped.map { |e| e[1]}
   end
 end
