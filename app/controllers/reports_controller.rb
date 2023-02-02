dbcc10b30061515b7a3fa3b58b7cb0e69bbfae26
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-02 13:43:47 +0200

    fixed in reports controller, added group and sum

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,16 +19,19 @@ class ReportsController < ApplicationController
   def report_by_dates
     @operations = Operation.where("odate >= :date_from AND odate <= :date_to",
                                   { date_from: params["filter"]["date-from"].to_date,
-                                    date_to: params["filter"]["date-to"].to_date
-                                  }
+                                    date_to: params["filter"]["date-to"].to_date }
     )
+    @operations_grouped = @operations.group("strftime('%Y-%m-%d', odate)").sum("amount")
+     # .group("strftime('%Y-%m-%d 00:00:00 UTC', created_at, '-6 days', 'weekday 0')")
     create_report
   end
 
   private
   def create_report
-    operations_data = @operations.map { |o| [o.amount, o.odate.strftime("%Y.%m.%d. %H:%M")] }
-    @amounts = operations_data.map { |e| e[0]}
-    @dates = operations_data.map { |e| e[1]}
+    # operations_data = @operations.map { |o| [o.amount, o.odate] }
+    # @amounts = operations_data.map { |e| e[0]}
+    # @dates = operations_data.map { |e| e[1]}
+    @amounts = @operations_grouped.map { |e| e[1]}
+    @dates = @operations_grouped.map { |e| e[0]}
   end
 end
