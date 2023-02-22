a3e2244ca70dc9e9aeb0f2a609a85cbc01d4f080
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-22 11:49:23 +0200

    Added translations, fixed styles

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module FinanceApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.available_locales = %i[en ua]
+    config.i18n.available_locales = %i[en uk]
     config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
