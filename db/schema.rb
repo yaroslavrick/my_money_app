a8cd03b98275352b4ceef8b31278c0e96666baa0
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-03-03 11:36:57 +0200

    Added setting for deployement t orailway.app

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,27 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_116_103_618) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_103618) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_categories_on_name', unique: true
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
-  create_table 'operations', force: :cascade do |t|
-    t.decimal 'amount'
-    t.datetime 'odate'
-    t.string 'description'
-    t.bigint 'category_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['category_id'], name: 'index_operations_on_category_id'
+  create_table "operations", force: :cascade do |t|
+    t.decimal "amount"
+    t.datetime "odate"
+    t.string "description"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_operations_on_category_id"
   end
 
-  add_foreign_key 'operations', 'categories'
+  add_foreign_key "operations", "categories"
 end
